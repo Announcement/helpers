8ef736e40925bada1cd9a8a98dc210e497f955f2
Jacob Francis Powers <xxv9xx@gmail.com>
2017-02-13 17:52:39 -0600

    provide appropriete test cases for curry

---
+++ test/test.curry.js
@@ -0,0 +1,26 @@
+import curry from '../src/curry.js'
+import {expect} from 'chai'
+
+describe('curry', function () {
+
+  function f(x, y) { return x + y }
+  let g = (x, y) => x * y
+
+  let f1 = curry(f)
+  let g1 = curry(g)
+
+  it('should still be callable while waiting for parameters', function () {
+    let f2 = f1(1);
+    expect(f2).to.be.a('function')
+  })
+
+  it('should return the expected result', function () {
+    let z = f1(2, 3)
+    expect(z).to.be.eql(5)
+  })
+
+  it('should also work with arrow functions', function () {
+    let z = g1(2, 3)
+    expect(z).to.be.eql(6)
+  })
+})
